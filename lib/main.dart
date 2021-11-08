75d51e6e8129f1c053a3f8fd1db7aec2846f4d98
Huzaifa Zahoor <huzaifazahoorpro@gmail.com>
2021-11-08 12:00:00 -0800

    add keys and sounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,8 +21,41 @@ class MyApp extends StatelessWidget {
 class Xylophone extends StatelessWidget {
   const Xylophone({Key? key}) : super(key: key);
 
+  Widget buildKey({required Color color, required int noteNumber}) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          playSound(noteNumber);
+        },
+        child: Container(
+          color: color,
+        ),
+      ),
+    );
+  }
+
+  void playSound(int noteNumber) {
+    final player = AudioCache();
+    player.play('assets_note$noteNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+            buildKey(color: Colors.amber, noteNumber: 1),
+            buildKey(color: Colors.red, noteNumber: 2),
+            buildKey(color: Colors.blue, noteNumber: 3),
+            buildKey(color: Colors.purple, noteNumber: 4),
+            buildKey(color: Colors.green, noteNumber: 5),
+            buildKey(color: Colors.pink, noteNumber: 6),
+            buildKey(color: Colors.deepOrange, noteNumber: 7),
+          ],
+        ),
+      ),
+    );
   }
 }
